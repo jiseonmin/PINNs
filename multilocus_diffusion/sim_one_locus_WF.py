4678bd827b62382da808b68ed9ce9a9a0ba27332
jiseonmin <minjiseon0827@gmail.com>
2023-04-19 23:16:44 +0000

    save f, t data without estimating p(f,t)

--- multilocus_diffusion/sim_one_locus_WF.py
+++ multilocus_diffusion/sim_one_locus_WF.py
@@ -14,54 +14,50 @@ args = parser.parse_args()
 
 def simulate_WF(N, n0, s):
     # initial size of the lineage = n0
+    fseries = []
+    tseries = []
     n = n0
     t = 0
+    fseries.append(n/N)
+    tseries.append(t)
     while n > 0 and n < N:
         n = np.random.binomial(N, n*(1+s) / (N-n + n*(1+s)))
         t += 1
-    return t, n
+        fseries.append(n/N)
+        tseries.append(t)
+    return fseries, tseries
 
 if __name__ == '__main__':
-    tlist = []
-    nlist = []
-    
+    fendlist = []
+    tendlist = []
+    data = {'f': [], 't': []}
     n0 = int(args.f0 * args.N)
     nsim = 0
     while nsim < args.nsim:
-        t_and_n = simulate_WF(args.N, n0, args.s)
-        tlist.append(t_and_n[0])
-        nlist.append(t_and_n[1])
+        fseries, tseries = simulate_WF(args.N, n0, args.s)
+        data['f'].extend(fseries)
+        data['t'].extend(tseries)
         nsim += 1
+        tendlist.append(tseries[-1])
+        fendlist.append(fseries[-1])
 
-    nlist = np.array(nlist)
-    tlist = np.array(tlist)
-    # convert the data to be a series of p(f=1, t) and p(f=0, t)
-    print(nlist)
-    t_extinct_list = tlist[np.where(nlist == 0)[0]]
-    t_fix_list = tlist[np.where(nlist != 0)]
-    print(t_fix_list)
+    np.save("single_locus_FP_data.npy", data)
+    print(np.max(tendlist))
+    fmatrix = np.broadcast_to(fendlist, (np.max(tendlist) + 1, len(fendlist)))
+    tmatrix = np.broadcast_to(range(np.max(tendlist) + 1), (len(fendlist), np.max(tendlist) + 1)).T
+    print(fmatrix.shape)
+    fmatrix.flags.writeable = True
+    fmatrix2 = fmatrix.copy()
+    t0array = np.argwhere(np.array(data['t']) == 0).flatten()
+    print(t0array)
+    print(np.array(data['f'])[t0array])
+    for i in range(len(t0array) - 1):
+        print(i)
+        t0 = t0array[i]
+        tend = t0array[i+1]
+        fmatrix2[0:tend-t0, i] = np.array(data['f'][t0:tend])
+    last_tend = len(data['f'])
+    fmatrix2[0:last_tend-t0array[-1], -1] = np.array(data['f'][t0array[-1]:last_tend])
+    np.save("single_locus_FP_fmatrix.npy", fmatrix2)
 
-    # sort t extinct and tfix, the index value of t + 1 divided by nsim is the probability of fixation at that t.
-    t_extinct_list.sort()
-    p_extinct_list = np.arange(1, 1+len(t_extinct_list)) / nsim
-    t_fix_list.sort()
-    p_fix_list = np.arange(1, len(t_fix_list) + 1) / nsim
-
-    f_initial = np.append(np.arange(0, args.f0, args.f0 / 100), np.arange(args.f0 * 101/100, 1, args.f0/100))
-    f_initial = np.append(np.array([args.f0]), f_initial)
-    p_initial = np.zeros(len(f_initial) - 1)
-    p_initial = np.append(np.array([1]), p_initial)    
-    data_dict = {'f':np.concatenate((np.zeros(len(t_extinct_list)), np.ones(len(t_fix_list)), f_initial), axis=None), 
-    't':np.concatenate((t_extinct_list, t_fix_list, np.zeros(len(f_initial))), axis=None), 
-    'phi':np.concatenate((p_extinct_list, p_fix_list, p_initial), axis=None)}
-
-    np.save("single_locus_data.npy", data_dict)
-    print(data_dict)
-
-    print(data_dict['phi'][0])
-    print(len(data_dict['phi']))
-    ######### todo - get dictionary of phi, f=1 or 0, t. Save as npy file
-    ######### in mathematical side, I need to think of how to go from simulated f(t) to p(f,t) (sparse, noisy data)
-    ############ --> the quality of p(f, t) is all bad unless nsim is very large. But I can stil pick some points (f_i, t_i)
-    ## to make as good point estimate as possible based on the f(t)'s (e.g. near equilibria at relatively large t (t > 1/s))
-
+## A row in fmatrix represent a snapshot of allele frequency at i-th time point.
