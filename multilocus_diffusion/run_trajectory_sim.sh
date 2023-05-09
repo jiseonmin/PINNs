a36bb50e885bbf59db588f3ca3672697961977c2
jiseonmin <minjiseon0827@gmail.com>
2023-05-09 22:25:00 +0000

    building 2-locus PINN

--- multilocus_diffusion/run_trajectory_sim.sh
+++ multilocus_diffusion/run_trajectory_sim.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-for i in {1..10}
+for i in {1..20}
 do
     echo "Simulation $i"
-    slim freq_trajectory_sim.slim > $i.part &
+    slim -d S1=-1e-5 -d S2=-1e-5 -d MU1=1e-8 -d MU2=1e-8 freq_trajectory_sim.slim > $i.part &
 done
 wait
 echo "SLiMulation done"
 awk '/1 0.0/,0' 1.part > two_locus_trajectory.csv
 
-for i in {2..10}
+for i in {2..20}
 do
     awk '/1 0.0/,0' $i.part >> two_locus_trajectory.csv
 done
